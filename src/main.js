a20600f3d71dd83ac1c07439ed69feffb304de4c
Maksim Eremin <ermak.dev@gmial.com>
2019-08-14 18:16:46 +0400

    верстка страницы списка задач

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { 
+  faEllipsisH, 
+  faTimes, 
+  faPlus,
+  faPen } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+ 
+library.add(faEllipsisH)
+library.add(faTimes)
+library.add(faPen)
+library.add(faPlus)
+ 
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 import BootstrapVue from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
