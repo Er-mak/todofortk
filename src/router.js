52159397b3871c74683bbddc6e0ab81ad80fd428
ErMak <ermak.dev@gmail.com>
2019-08-14 13:56:09 +0400

    init todo app for tk

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import ToDoListPage from './views/ToDoListPage.vue'
 
 Vue.use(Router)
 
@@ -10,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'todolistpage',
+      component: ToDoListPage
     },
     {
       path: '/about',
---
+++ src/views/ToDoListPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="todolistpage">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <ToDoList msg="Welcome to ToDoList"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ToDoList from '@/components/ToDoList.vue'
+
+export default {
+  name: 'todolistpage',
+  components: {
+    ToDoList
+  }
+}
+</script>
